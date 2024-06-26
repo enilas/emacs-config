cb3392f99ca522d88cd8cd5a85ac3e7a5ad29404
PN <paulng91@gmail.com>
2024-06-25 19:18:08 -0700

    Latest init.el

--- init.el
+++ init.el
@@ -71,6 +71,37 @@
 ;; no-update-autoloads: t
 ;; End:
 
+;; Org ------------------------
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (shell      . t)
+   (python     . t)
+   (sqlite     . t)
+   (sql        . t)
+   (clojure    . t)))
+
+(setq org-src-window-setup                 'current-window
+      org-src-fontify-natively             t
+      org-confirm-babel-evaluate           nil
+      org-src-preserve-indentation         t
+      org-auto-align-tags                  nil
+      org-tags-column                      0
+      org-special-ctrl-a/e                 t
+      org-insert-heading-respect-content   t
+      org-hide-emphasis-markers            t
+      org-pretty-entities                  t
+      org-startup-indented                 t
+      org-babel-sh-command                 "bash"
+      prettify-symbols-unprettify-at-point t
+      org-log-into-drawer                  t)
+
+;; Auto-Save
+
+(defvar autosave-dir (concat "~/.emacs.d/auto-save" "/"))
+(make-directory autosave-dir t)
+
 ;; UI --------------------------
 
 (use-package cyberpunk-theme
@@ -108,6 +139,7 @@
 
 (elpaca dashboard
   (use-package dashboard
+    :after elpaca
     :ensure t
     :config
     (dashboard-setup-startup-hook)))
@@ -137,6 +169,9 @@
 		    :config
 		    (add-hook 'prog-mode-hook #'ws-butler-mode)))
 
+(elpaca which-key (use-package which-key
+		    :init (which-key-mode +1)))
+
 (elpaca helpful (use-package helpful
 		  :config
 		  ;; Note that the built-in `describe-function' includes both functions
@@ -163,8 +198,30 @@
 		  :init
 		  (vertico-mode)))
 
-(elpaca marginalia (use-package marginalia))
-(elpaca consult (use-package consult))
+(elpaca marginalia (use-package marginalia
+		     :after vertico))
+(elpaca consult (use-package consult
+		  :hook (completion-list-mode . consult-preview-at-point-mode)
+		  :ensure t
+		  :init
+		  (setq register-preview-delay         0.5
+			register-preview-foncution     #'consult-register-format
+			xref-show-xrefs-function       #'consult-xref
+			xref-show-definitions-function #'consult-xref
+			consult-narrow-key             "<")
+		  (advice-add #'register-preview :override #'consult-register-window)
+		  :config
+		  (dolist (matcher '("^magit-process*" "^\*EGLOT-*"))
+		    (push mtacher consult-buffer-filter))
+		  (consult-customize
+		   consult-theme :preview-key '(:debounce 0.2 any)
+		   consult-ripgrep consult-git-grep consult-grep
+		   consult-bookmark consult-recent-file consult-xref
+		   consult--source-bookmark consult--source-file-register
+		   consult--source-recent-file consult--source-project-recent-file
+		   :preview-key '(:debounce 0.4 any))
+		  (autoload 'projectile-project-root "~/repos/")
+		  (setq consult-project-function (lambda (_) (projectile-project-root)))))
 (elpaca orderless ;; Optionally use the `orderless' completion style.
   (use-package orderless
   :init
@@ -177,25 +234,82 @@
 (elpaca embark (use-package embark))
 (elpaca embark-consult (use-package embark-consult))
 (elpaca swiper (use-package swiper))
+(elpaca company (use-package company
+		  :ensure t
+		  :init (global-company-mode)))
+
+(elpaca async (use-package async
+		:init
+		(dired-async-mode +1)
+		(async-bytecomp-package-mode +1)))
+(elpaca csv-mode (use-package csv-mode
+		   :mode ("\\.csv?\\'" . csv-mode)
+		   :hook (csv-mode . csv-align-mode)))
+(elpaca yaml-mode (use-package yaml-mode))
+(elpaca json-mode (use-package json-mode))
+(elpaca expand-region (use-package expand-region))
+(elpaca undo-tree (use-package undo-tree
+		    :custom (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+		    :config (global-undo-tree-mode +1)))
+(elpaca exec-path-from-shell (use-package exec-path-from-shell
+			       :init (when (daemonp) (exec-path-from-shell-initialize))))
 
 ;; Development -----------------
 
 (electric-pair-mode 1)
 (show-paren-mode 1)
 
+(elpaca eat (use-package eat))
+(elpaca hydra (use-package hydra))
+(elpaca vdiff (use-package vdiff
+		:config (define-key vdiff-mode-map (kbd "C-c") vdiff-mode-prefix-map)))
+
 (elpaca transient (use-package transient))
 (elpaca git (use-package git))
 (elpaca git-commit (use-package git-commit))
 (elpaca tree-sitter (use-package tree-sitter))
 (elpaca magit (use-package magit))
+(elpaca forge (use-package forge
+		:config
+		(add-to-list 'forge-alist
+			     '("gitlab.sig-gis.com"
+			       "gitlab.sig-gis.com/api/v4"
+			       "gitlab.sig-gis.com"
+			       forge-gitlab-repository))))
+
 (elpaca inflections (use-package inflections))
+
+(elpaca paredit (use-package paredit))
+
+(elpaca flycheck (use-package flycheck
+		   :ensure t
+		   :init (global-flycheck-mode)))
+(elpaca flycheck-clj-kondo (use-package flycheck-clj-kondo
+			     :ensure t))
 
 (elpaca projectile (use-package projectile
 		     :config
 		     (projectile-mode +1)
-		     (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)))
+		     (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+		     :custom
+		     (projectile-project-search-path          '("~/repos")
+		      projectile-globally-ignored-directories '("~/")
+		      projectile-enable-caching               't)))
 
-(elpaca clojure-mode (use-package clojure-mode))
+(defadvice projectile-on (around exlude-tramp activate)
+  "This should disable projectile when visiting a remote file"
+  (unless  (--any? (and it (file-remote-p it))
+                   (list
+                    (buffer-file-name)
+                    list-buffers-directory
+                    default-directory
+                    dired-directory))
+    ad-do-it))
+
+(elpaca clojure-mode (use-package clojure-mode
+		       :ensure t
+		       :config
+		       (require 'flycheck-clj-kondo)))
 ;; People are working on replacing with clojure-ts-mode. Feature incomplete at the moment.
 (elpaca clojure-ts-mode (use-package clojure-ts-mode))
 (elpaca clojure-snippets (use-package clojure-snippets))
@@ -209,11 +323,134 @@
 (elpaca clj-refactor (use-package clj-refactor
 		       :config
 		       (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)))
-(elpaca cider (use-package cider))
+(elpaca cider (use-package cider
+		:config
+		(cider-enable-flex-completion)
+		(require 'cider)
+		:custom
+		(cider-repl-pop-to-buffer-on-connect         'display-only)
+		(cider-eldoc-display-for-symbol-at-point     t)
+		(cider-eldoc-display-context-dependent-info  t)
+		(nrepl-hide-special-buffers                  t)
+		(cider-repl-display-help-banner              nil)
+		(cider-repl-use-pretty-printing              t)
+		(cider-repl-use-clojure-font-lock            t)
+		(cider-repl-wrap-history                     t)
+		(cider-repl-history-size                     5000)
+		(cider-repl-history-file                     "~/.emacs.d/cider/history")
+		(nrepl-use-ssh-fallback-for-remote-hosts     t)
+		(cider-overlays-use-font-lock                t)
+		(cider-repl-buffer-size-limit                10000)
+		(cider-known-endpoints                       '(("localhost")))))
+(elpaca rainbow-delimiters (use-package rainbow-delimiters
+			     :ensure t))
+
+(elpaca tree-sitter-ess-r (use-package tree-sitter-ess-r
+			    :ensure t
+			    :config
+			    (add-hook 'ess-r-mode-hook 'tree-sitter-ess-r-mode-activate)))
+
+;; Emacs Is Web Ready
+
+(elpaca elpher (use-package elpher
+  :custom
+  (elpher-gemini-max-fill-width  120)
+  (elpher-gemini-TLS-cert-checks nil)
+  (elpher-ipv4-always            t)
+  (elpher-open-urls-with-eww     t)
+  (elpher-default-url-type       "gemini")))
+
+;; tramp settings
+
+(setq remote-file-name-inhibit-cache nil)
+(setq vc-ignore-dir-regexp
+      (format "%s\\|%s"
+                    vc-ignore-dir-regexp
+                    tramp-file-name-regexp))
+(setq tramp-verbose 1)
+
+;; ElDoc
+
+(elpaca eldoc)
+(use-package eldoc
+  :custom (eldoc-echo-area-use-multiline-p t))
+;; (use-package eldoc-box)
+
+;; Completion
+;; Corfu & Dabbrev
+
+;; Completion popups!
+(elpaca corfu)
+(use-package corfu
+  :straight (:files (:defaults "extensions/*"))
+  :init
+  (global-corfu-mode)
+  :config
+  (set-face-attribute 'corfu-default nil :height 1.0)
+  :hook (corfu-mode . corfu-popupinfo-mode)
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-delay 0.5)
+  (corfu-auto-prefix 2)
+  (corfu-separator ?\s)
+  (corfu-quit-at-boundary nil)
+  (tab-always-indent 'complete)
+  (corfu-popupinfo-delay 0.2)
+  (completion-category-overrides '((eglot (styles orderless)))))
+
+  (add-hook 'corfu-popupinfo-mode-hook (lambda () (set-face-attribute 'corfu-popupinfo nil :height 1.0)))
+
+(elpaca cape)
+(use-package cape
+  ;; Bind dedicated completion commands
+  ;; Alternative prefix keys: C-c p, M-p, M-+, ...
+  :bind (("M-p p" . completion-at-point) ;; capf
+         ("M-p t" . complete-tag)        ;; etags
+         ("M-p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("M-p h" . cape-history)
+         ("M-p f" . cape-file)
+         ("M-p k" . cape-keyword)
+         ("M-p s" . cape-symbol)
+         ("M-p a" . cape-abbrev)
+         ("M-p i" . cape-ispell)
+         ("M-p l" . cape-line)
+         ("M-p w" . cape-dict)
+         ("M-p \\" . cape-tex)
+         ("M-p _" . cape-tex)
+         ("M-p ^" . cape-tex)
+         ("M-p &" . cape-sgml)
+         ("M-p r" . cape-rfc1345))
+  :init
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file))
+
+(elpaca dabbrev)
+
+;; Use Dabbrev with Corfu!
+(use-package dabbrev
+    ;; Swap M-/ and C-M-/
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
+  ;; Other useful Dabbrev configurations.
+  :custom (dabbrev-ignored-buffer-regexps
+           '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
+
 
 (global-set-key [C-mouse-4] 'text-scale-increase)
 (global-set-key [C-mouse-5] 'text-scale-decrease)
+(global-set-key (kbd "C-s") 'swiper)
 
+(custom-set-variables
+ '(global-display-line-numbers-mode t))
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-ts-mode-hook #'cider-mode)
+(add-hook 'clojure-ts-mode-hook #'enable-paredit-mode)
+(add-hook 'clojure-ts-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-ts-mode-hook #'clj-refactor-mode)
+
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
 
 ;; A few more useful configurations...
 (use-package emacs
